8ca79e06a524baa4fadddbdeedd60cc27b7c2093
PenJR <152262578+PenJR@users.noreply.github.com>
2024-11-25 15:28:32 +0800

    Update Data_Storage.py

--- Data_Storage.py
+++ Data_Storage.py
@@ -3,7 +3,7 @@ import json
 class DataStorage:
   def save(libary):
     with open ('Library.json','w') as file:
-      file = json.dump(data,file, indent = 4)
+      file = json.dump(library,file, indent = 4)
 
   def load(library):
     with open ('Libray.json','r') as file:
