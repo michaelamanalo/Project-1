591bca48023236889912df9b3c51c6efd300c46c
ABEJARON <abejaroncharlesderek@gmail.com>
2024-11-28 20:25:28 +0800

    First4

--- Queues.py
+++ Queues.py
@@ -1,3 +1,5 @@
+import random
+
 class Node:
     def __init__(self, track):
         self.track = track
@@ -27,7 +29,6 @@ class List:
         self.size = 0
 
 
-
 class Queues:
     def __init__(self):
         self.list = List()
@@ -37,18 +38,53 @@ class Queues:
         self.paganation = 10
         self.total_duration = 0
 
-    def play(self):      
-        pass
-    
-    def skip(self):     
-        pass
-    
+    def play(self):
+        if self.shuffle:
+            shuffled_queue = random.sample(self.queue, len(self.queue))
+            self.current_index = 0
+            print(f"Playing: {shuffled_queue[self.current_index]}")
+        elif self.current_index is None:
+            print("No track is currently playing.")
+        else:
+            print(f"Playing: {self.queue[self.current_index]}")
+
+
+    def skip(self):
+        if self.current is None:
+            print("No track is currently playing.")
+            return
+        
+        if self.current.next is None and not self.repeat:
+            print("End of the queue. No more tracks to skip to.")
+            return
+        
+        if self.repeat:
+            self.current = self.list.head.track
+            print(f"Playing: {self.current}")
+        else:
+            self.current = self.current.next.track if self.current.next else None
+            if self.current:
+                print(f"Playing: {self.current}")
+            else:
+                print("End of the queue.")
+
     def previous(self):
-        pass
-    
+        if self.current is None:
+            print("No track is currently playing.")
+            return
+        
+        if self.current.prev is None:
+            print("No previous track. You are at the start of the queue.")
+            return
+        
+        self.current = self.current.prev.track
+        print(f"Playing: {self.current}")
+
     def toggle_shuffle(self):
-        pass
-    
+        self.shuffle = not self.shuffle
+        status = "enabled" if self.shuffle else "disabled"
+        print(f"Shuffle is now {status}.")
+
     def toggle_repeat(self):
         pass
     
@@ -60,8 +96,3 @@ class Queues:
     
     def exit(self):
         pass
-
-    
-
-
-
